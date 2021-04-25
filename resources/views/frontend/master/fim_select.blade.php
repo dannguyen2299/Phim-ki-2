162acc3ded49e5c279798b916cf399efffee2bb8
Dan <dannguyen22993@gmail.com>
2021-04-25 20:17:48 +0700

    Dan-Fim

---
+++ resources/views/frontend/master/fim_select.blade.php
@@ -0,0 +1,76 @@
+
+<div class="col-lg-4" >
+    <div class="row">
+      <h5 class="text-light ml-3">PHIM ĐÁNH DẤU</h5>
+    </div>
+    <div class="row">
+      <div class="col-12">
+          <h6 class="text-danger">Tên phim</h6>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+         <table class="table">
+           <tr>
+             <td><a href=""><p class="text-light mb-0">Hoa và nắng</p></a></td>
+             <td><a href=""><p class="text-light mb-0">Tập 5</p></a></td>
+             <td><a href=""><i class="fa fa-times mb-0 text-danger"></i></td>
+           </tr>
+           <tr>
+            <td><a href=""><p class="text-light mb-0">Hoa và nắng</p></a></td>
+            <td><a href=""><p class="text-light mb-0">Tập 5</p></a></td>
+            <td><a href=""><i class="fa fa-times mb-0 text-danger"></i></td>
+          </tr>
+          <tr>
+            <td><a href=""><p class="text-light mb-0">Hoa và nắng</p></a></td>
+            <td><a href=""><p class="text-light mb-0">Tập 5</p></a></td>
+            <td><a href=""><i class="fa fa-times mb-0 text-danger"></i></td>
+          </tr>
+          <tr>
+            <td><a href=""><p class="text-light mb-0">Hoa và nắng</p></a></td>
+            <td><a href=""><p class="text-light mb-0">Tập 5</p></a></td>
+            <td><a href=""><i class="fa fa-times mb-0 text-danger"></i></td>
+          </tr>
+         </table>
+         <a class=" text-success" href="">Xem thêm</a>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <h5 class="text-danger ml-3">HÔM NAY XEM GÌ ?</h5>
+    </div>
+    <div class="row-2">
+      <small class="text-secondary">Nếu bạn đang phân vân không biết xem gì. Hãy để hệ thống chúng tôi lựa chọn ngẫu nhiên cho bạn.<br> Chúc bạn có 1 ngày xem phim vui vẻ</small>
+    </div>
+    <div class="row-2">
+      <button class="btn bg-danger mt-2">Lựa chọn của hệ thống</button>
+    </div>
+    <div class="row mt-3">
+      <h5 class="text-danger ml-3">TOP PHIM IDMI ?</h5>
+    </div>
+    <div class="row-2 mt-3">
+      <table class="table table-dark">
+        <tr>
+          <td><a href=""><i class="fa fa-arrow-up text-danger"></i></a></td>
+          <td><a href=""><p class="text-danger mb-0">Hoa và nắng</p></a></td>
+          <td><a href="" class="text-light">9.8 sao</td>
+        </tr>
+        <tr>
+          <td><a href=""><i class="fa fa-arrow-down text-danger"></i></a></td>
+          <td><a href=""><p class="text-danger mb-0">Hoa và nắng</p></a></td>
+          <td><a href="" class="text-light">9.8 sao</td>
+        </tr>
+        <tr>
+          <td><a href=""><i class="fa fa-arrow-down text-danger"></i></a></td>
+          <td><a href=""><p class="text-danger mb-0">Hoa và nắng</p></a></td>
+          <td><a href="" class="text-light">9.8 sao</td>
+        </tr>
+        <tr>
+          <td><a href=""><i class="fa fa-arrow-up text-success"></i></a></td>
+          <td><a href=""><p class="text-danger mb-0">Hoa và nắng</p></a></td>
+          <td><a href="" class="text-light">9.8 sao</td>
+        </tr>
+       
+      </table>
+    </div>
+
+  </div>
