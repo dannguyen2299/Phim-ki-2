0e23a94b63246a42f1399cbb2df043fcbaf2b2a7
thanhkdit <kieudangthanh123@gmail.com>
2021-05-13 21:41:34 +0700

    Thanh: sua linh tinh, ko loi

--- database/seeds/episode.php
+++ database/seeds/episode.php
@@ -13,11 +13,11 @@ class episode extends Seeder
     {
         DB::table('episode')->delete();
         DB::table('episode')->insert([
-            ['episode_id'=>1,'episode_name'=>'Nooooooooooo','url_first'=>'a.com','url_second'=>'b.com','status'=>1,'movie_id'=>1],
-            ['episode_id'=>2,'episode_name'=>'Nooooooooooo','url_first'=>'a.com','url_second'=>'b.com','status'=>0,'movie_id'=>2],
-            ['episode_id'=>3,'episode_name'=>'Nooooooooooo','url_first'=>'a.com','url_second'=>'b.com','status'=>1,'movie_id'=>3],
-            ['episode_id'=>4,'episode_name'=>'Nooooooooooo','url_first'=>'a.com','url_second'=>'b.com','status'=>0,'movie_id'=>4],
-            ['episode_id'=>5,'episode_name'=>'Nooooooooooo','url_first'=>'a.com','url_second'=>'b.com','status'=>1,'movie_id'=>1],
+            ['episode_id'=>1,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>1],
+            ['episode_id'=>2,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>0,'movie_id'=>2],
+            ['episode_id'=>3,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>3],
+            ['episode_id'=>4,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>0,'movie_id'=>4],
+            ['episode_id'=>5,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>1],
         ]);
         //
     }
