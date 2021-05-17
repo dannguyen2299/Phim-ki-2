737576ce2510c601ef817cd27f8a4e3757676357
thanhkdit <kieudangthanh123@gmail.com>
2021-05-17 23:59:53 +0700

    Linh tinh

--- database/seeds/episode.php
+++ database/seeds/episode.php
@@ -13,11 +13,11 @@ class episode extends Seeder
     {
         DB::table('episode')->delete();
         DB::table('episode')->insert([
-            ['episode_id'=>1,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>1],
-            ['episode_id'=>2,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/embed/h1ajyQo3oaM','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>0,'movie_id'=>2],
-            ['episode_id'=>3,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/embed/KVHimR1vf0o','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>3],
-            ['episode_id'=>4,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/embed/H96ceHhGtD8','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>0,'movie_id'=>4],
-            ['episode_id'=>5,'episode_name'=>'Nooooooooooo','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/watch?v=RhPIciEZetE&ab_channel=MixiGaming','status'=>1,'movie_id'=>1],
+            ['episode_id'=>1,'episode_name'=>'Tập 1','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/embed/Ws-QlpSltr8','status'=>1,'movie_id'=>1],
+            ['episode_id'=>2,'episode_name'=>'Tập 2','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/embed/Ws-QlpSltr8','status'=>0,'movie_id'=>2],
+            ['episode_id'=>3,'episode_name'=>'Tập 3','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/embed/Ws-QlpSltr8','status'=>1,'movie_id'=>3],
+            ['episode_id'=>4,'episode_name'=>'Tập 4','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/embed/Ws-QlpSltr8','status'=>0,'movie_id'=>4],
+            ['episode_id'=>5,'episode_name'=>'Tập 5','url_first'=>'https://www.youtube.com/embed/Ws-QlpSltr8','url_second'=>'https://www.youtube.com/embed/Ws-QlpSltr8','status'=>1,'movie_id'=>1],
         ]);
         //
     }
