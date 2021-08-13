c9745b7d97fe88adc39b4366795bf3042486c9e2
tuananh <tuananhtybg.99@gmail,com>
2021-08-13 16:26:05 +0700

    Ẩn category , nation

--- app/Http/Controllers/frontend/NavController.php
+++ app/Http/Controllers/frontend/NavController.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\Controller;
 class NavController extends Controller
 {
     public function getNation(){
-        return $data['nation'] = DB::table('nation')->get();
+        return $data['nation'] = DB::table('nation')->where('status','1')->get();
     }
     public function getCategory(){
-        return $data['category_l'] = DB::table('category')->get();
+        return $data['category_l'] = DB::table('category')->where('status','1')->get();
     }
 }
